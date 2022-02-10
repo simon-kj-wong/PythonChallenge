25f5c2f340d67d2fe388301f2f1f4ad8efc3f427
Simon Wong <simon.kj.wong@gmail.com>
2022-02-10 15:14:32 +1100

    finished part3

--- part3.py
+++ part3.py
@@ -1,6 +1,8 @@
+import re
 print("answer to part3")
 f = open("part3.txt", 'r')
 content = f.read()
-content.strip()
-
+print("".join(re.findall(r'[^A-Z+][A-Z]{3}([a-z])[A-Z]{3}[^A-Z+]', content)))
 f.close()
+print("take to http://www.pythonchallenge.com/pc/def/linkedlist.html")
+print("which directs to http://www.pythonchallenge.com/pc/def/linkedlist.php")
